e3ac5b2cf0583050a6948d998e0710fb51e67c10
Edixon <payruz16@gmail.com>
2021-05-11 18:40:48 -0500

    orden en el codigo

--- core/app/action/Cita/AddCitaLavado-action.php
+++ core/app/action/Cita/AddCitaLavado-action.php
@@ -13,54 +13,54 @@ $cliente=ClienteData::getByCC($cedula);
 
 if ($temp>$fechaactual){
 
-if ($cliente!=NULL ){
+   if ($cliente!=NULL ){
 
-$cita= new CitaData();
+   $cita= new CitaData();
 
-$cita->idcliente=$cliente->id;
+   $cita->idcliente=$cliente->id;
 
-$cita->estado=0;
+   $cita->estado=0;
 
-$cita->fechapedida=date("Y-m-d H:i:s", strtotime($_POST["hora"]));
+   $cita->fechapedida=date("Y-m-d H:i:s", strtotime($_POST["hora"]));
 
-$aux=$cita->add();
-
-
-if($aux[0]==1){
-   core::alert("Su lavado ha ingresado con exito al sistema");
-   
-
-   $cliente=ClienteData::getByCC($cedula);
-
- 
-   $mensaje="EL CLIENTE *".$cliente->nombre." ".$cliente->apellido."* SOLICITA UN LAVADO PARA LAS *".$cita->fechapedida."*"; // Message
-  
-   $api=new ApiData();
-   
-   $api->enviarMensajeAdmin($mensaje);
-   $api->enviarMensajeGeneral($mensaje,3015256417);
-   //VUE JS
-
-   
-   Core::redir("./?view=Cita/UserCita");
-
- 
-}else{
-
-   core::alert("Error al ingresar su Cita ");
-   
-   Core::redir("./?view=Cita/UserCita");
-
-}
+   $aux=$cita->add();
 
 
-}else {
+      if($aux[0]==1){
+         core::alert("Su lavado ha ingresado con exito al sistema");
+         
+      
+         $cliente=ClienteData::getByCC($cedula);
+      
+      
+         $mensaje="EL CLIENTE *".$cliente->nombre." ".$cliente->apellido."* SOLICITA UN LAVADO PARA LAS *".$cita->fechapedida."*"; // Message
+      
+         $api=new ApiData();
+         
+         $api->enviarMensajeAdmin($mensaje);
+         $api->enviarMensajeGeneral($mensaje,3015256417);
+         //VUE JS
+      
+         
+         Core::redir("./?view=Cita/UserCita");
+      
+      
+      }else{
+      
+         core::alert("Error al ingresar su Cita ");
+         
+         Core::redir("./?view=Cita/UserCita");
+      
+      }
+      
+      
+   }else {
 
-   core::alert("Usuario no registrado ");
+      core::alert("Usuario no registrado ");
 
-   print "<script>window.location='index.php?view=Cita/UserAddCliente&cedula=".$cedula."';</script>";
+      print "<script>window.location='index.php?view=Cita/UserAddCliente&cedula=".$cedula."';</script>";
 
-}
+   }
 
 
 }else{
